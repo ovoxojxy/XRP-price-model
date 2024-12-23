c4356be98892c52079065064045602ed374592d4
Jarod <jjarod517@gmail.com>
2024-12-23 13:11:49 -0500

    Created strategy format and tested with S&P 500 holdings

--- Backtesting/backtesting_SP.py
+++ Backtesting/backtesting_SP.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('data_path')
+df = pd.read_csv('/Users/jay/Desktop/XRP-price-model/Backtesting/data/S&P-500-Historical-Data.csv')
 
 df['Date'] = pd.to_datetime(df['Date'])
 
@@ -14,25 +14,25 @@ buy_threshold = -.04
 sell_threshold = .06
 position = 0
 cash = initial_capital
-ASSET_holdings = 0
+SP_holdings = 0
 
 for i in range(1, len(df)):
     #calculate percentage change from the previous day
     pct_change = float(df['Change-%'][i][:-1])
 
     if position == 0 and pct_change <= buy_threshold:
-        #buy ASSET
-        ASSET_holdings = cash / float(df['Price'][i])
+        #buy SP
+        SP_holdings = cash / float(df['Price'][i])
         cash = 0
         position = 1
-        print(f"Bought ASSET on ${df['Date'][i]} at ${df['Price'][i]:.2f}")
+        print(f"Bought SP on ${df['Date'][i]} at ${df['Price'][i]:.2f}")
 
     elif position == 1 and pct_change >= sell_threshold:
-        #sell ASSET
-        cash = ASSET_holdings * df['Price'][i]
-        ASSET_holdings = 0
+        #sell SP
+        cash = SP_holdings * df['Price'][i]
+        SP_holdings = 0
         position = 0
-        print(f"Sold ASSET on ${df['Date'][i]} at ${df['Price'][i]:.2f}")
+        print(f"Sold SP on ${df['Date'][i]} at ${df['Price'][i]:.2f}")
 
-final_value = cash + ASSET_holdings * df['Price'].iloc[-1]
+final_value = cash + SP_holdings * df['Price'].iloc[-1]
 print(f"Final portfolio value: ${final_value:.2f}")
