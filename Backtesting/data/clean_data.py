0be426968010cc22576289557a79647451bd8e08
Jarod <jjarod517@gmail.com>
2024-12-23 17:25:44 -0500

    Removed unnecessary datasets

--- Backtesting/data/clean_data.py
+++ Backtesting/data/clean_data.py
@@ -1,7 +1,12 @@
 import pandas as pd
 from io import StringIO
 
-df = pd.read_csv('/Users/jay/Desktop/XRP-price-model/Backtesting/data/cleaned_xrp.csv')
+df = pd.read_csv('/Users/jay/Desktop/XRP-price-model/Backtesting/data/Nasdaq_50_Historical_Data.txt')
+
+
+df = df.drop(columns=['<TICKER>', '<PER>'], errors='ignore')
+
+
 
 
 df['<DATE>'] = df['<DATE>'].astype(str)
@@ -11,8 +16,9 @@ df['<DATETIME>'] = pd.to_datetime(df['<DATE>'] + df['<TIME>'], format='%Y%m%d%H%
 
 df = df.drop(columns=['<DATE>','<TIME>'])
 
-output_path = '/Users/jay/Desktop/XRP-price-model/Backtesting/data/cleaned_xrp.csv'
+
+output_path = '/Users/jay/Desktop/XRP-price-model/Backtesting/data/cleaned_NZ50.csv'
 df.to_csv(output_path, index=False)
 
-# print(df.head(1))
+
 
